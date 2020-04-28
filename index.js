10145cb60f8dc98876de018b3466eb0ec256b98a
= <=>
2020-04-28 11:14:23 -0500

    release 0.0.6

---
+++ index.js
@@ -0,0 +1,8 @@
+const validatePassword = require("./src/validatePassword");
+const validateEmail = require("./src/validateEmail");
+const constants = require("./src/constants");
+module.exports = {
+  validateEmail,
+  validatePassword,
+  constants,
+};
