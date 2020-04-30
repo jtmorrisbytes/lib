bd9f9168c4ee67f93a5db91afc2147208ee232be
= <=>
2020-04-30 06:17:12 -0500

    finished tests for error interface

---
+++ spec/Error.spec.js
@@ -0,0 +1,3 @@
+const ERROR = require("../src/Error");
+const checkInterface = require("./helpers/checkInterface");
+checkInterface(ERROR, ERROR.IError);
---
+++ spec/helpers/checkInterface.js
@@ -0,0 +1,17 @@
+module.exports = function checkInterface(module, interface) {
+  describe("Module Error", () => {
+    it("Should should have an interface", () => {
+      expect(interface).toBeDefined();
+      expect(interface).toEqual(jasmine.any(Object));
+    });
+    it("Should have its children conform to the interface", () => {
+      for (property in module) {
+        if (module[property] != interface) {
+          Object.keys(interface).forEach((key) => {
+            expect(module[property].hasOwnProperty(key)).toBeTruthy();
+          });
+        }
+      }
+    });
+  });
+};
